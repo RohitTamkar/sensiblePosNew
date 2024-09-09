c7b089849a0570eb30e58b98fe25695d9a7956f1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-09 04:23:42 +0000

    Updating to latest FlutterFlow output.

--- lib/grocery_windows/add_customer_grocery/add_customer_grocery_model.dart
+++ lib/grocery_windows/add_customer_grocery/add_customer_grocery_model.dart
@@ -9,6 +9,7 @@ import '/flutter_flow/form_field_controller.dart';
 import 'add_customer_grocery_widget.dart' show AddCustomerGroceryWidget;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
