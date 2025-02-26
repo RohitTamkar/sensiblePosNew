f7fa1cff195c4aacaa73d2898ecd41aed9d6cf88
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-26 04:16:28 +0000

    Updating to latest FlutterFlow output.

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'SensibleBizPOS',
       localizationsDelegates: [
         FFLocalizationsDelegate(),
