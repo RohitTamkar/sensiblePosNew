2016d2c84a926df1c5059539bbf0dc54cf2f6a12
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-04-17 05:20:43 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/display2/display2_model.dart
+++ lib/pages/display2/display2_model.dart
@@ -1,22 +1,16 @@
 import '/backend/backend.dart';
-import '/backend/schema/structs/index.dart';
-import '/components/discount_and_delivery_copy/discount_and_delivery_copy_widget.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_drop_down.dart';
-import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
 import 'dart:math';
 import 'dart:ui';
-import '/custom_code/actions/index.dart' as actions;
 import '/custom_code/widgets/index.dart' as custom_widgets;
 import '/flutter_flow/custom_functions.dart' as functions;
-import '/index.dart';
 import 'display2_widget.dart' show Display2Widget;
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
@@ -24,71 +18,17 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:text_search/text_search.dart';
 
 class Display2Model extends FlutterFlowModel<Display2Widget> {
-  ///  Local state fields for this page.
-
-  List<CategoryStructStruct> categoryHive = [];
-  void addToCategoryHive(CategoryStructStruct item) => categoryHive.add(item);
-  void removeFromCategoryHive(CategoryStructStruct item) =>
-      categoryHive.remove(item);
-  void removeAtIndexFromCategoryHive(int index) => categoryHive.removeAt(index);
-  void insertAtIndexInCategoryHive(int index, CategoryStructStruct item) =>
-      categoryHive.insert(index, item);
-  void updateCategoryHiveAtIndex(
-          int index, Function(CategoryStructStruct) updateFn) =>
-      categoryHive[index] = updateFn(categoryHive[index]);
-
-  List<ProductStructStruct> productHive = [];
-  void addToProductHive(ProductStructStruct item) => productHive.add(item);
-  void removeFromProductHive(ProductStructStruct item) =>
-      productHive.remove(item);
-  void removeAtIndexFromProductHive(int index) => productHive.removeAt(index);
-  void insertAtIndexInProductHive(int index, ProductStructStruct item) =>
-      productHive.insert(index, item);
-  void updateProductHiveAtIndex(
-          int index, Function(ProductStructStruct) updateFn) =>
-      productHive[index] = updateFn(productHive[index]);
-
-  List<TaxMasterRecord> taxcollection = [];
-  void addToTaxcollection(TaxMasterRecord item) => taxcollection.add(item);
-  void removeFromTaxcollection(TaxMasterRecord item) =>
-      taxcollection.remove(item);
-  void removeAtIndexFromTaxcollection(int index) =>
-      taxcollection.removeAt(index);
-  void insertAtIndexInTaxcollection(int index, TaxMasterRecord item) =>
-      taxcollection.insert(index, item);
-  void updateTaxcollectionAtIndex(
-          int index, Function(TaxMasterRecord) updateFn) =>
-      taxcollection[index] = updateFn(taxcollection[index]);
-
   ///  State fields for stateful widgets in this page.
 
-  // Stores action output result for [Custom Action - getProductlistHive] action in Display2 widget.
-  List<ProductStructStruct>? hiveProductList;
-  // Stores action output result for [Custom Action - getCategorylistHive] action in Display2 widget.
-  List<CategoryStructStruct>? categoryListHive;
-  // Stores action output result for [Custom Action - scanPrinter] action in Display2 widget.
-  bool? resDevice2Copy;
-  // Stores action output result for [Custom Action - connectDevice] action in Display2 widget.
-  bool? isConnected;
   // State field(s) for DropDown widget.
   String? dropDownValue;
   FormFieldController<String>? dropDownValueController;
-  // State field(s) for TextFieldsearch widget.
-  FocusNode? textFieldsearchFocusNode;
-  TextEditingController? textFieldsearchTextController;
-  String? Function(BuildContext, String?)?
-      textFieldsearchTextControllerValidator;
-  List<PartyRecord> simpleSearchResults = [];
 
   @override
   void initState(BuildContext context) {}
 
   @override
-  void dispose() {
-    textFieldsearchFocusNode?.dispose();
-    textFieldsearchTextController?.dispose();
-  }
+  void dispose() {}
 }
